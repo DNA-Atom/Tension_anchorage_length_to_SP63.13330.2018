e166a915f753da37b992c772c8e04c48f86a43f7
Mikhail <rep-maik@mail.ru>
2022-02-15 23:21:25 +0300

    Add files via upload

--- command_again.py
+++ command_again.py
@@ -1,6 +1,9 @@
+import os
+
 def compute_again():
     proceed = input("\nВведите 1, что бы повторить программу или 0, что бы выключить программу    ")
     if proceed == '1':
+        os.system('cls||clear')
         return True
     elif proceed == '0':
         return False
@@ -9,6 +12,7 @@ def compute_again():
             proceed_error = input("\nВы ввели неправильную команду, повторите действие:"
                                       "\nВведите 1, что бы повторить программу или 0, что бы выключить программу    ")
             if proceed_error == '1':
+                os.system('cls||clear')
                 return True
             elif proceed_error == '0':
                 return False
--- dictionaries_and_functions_for_anker.py
+++ dictionaries_and_functions_for_anker.py
@@ -1,5 +1,6 @@
 import math
 import dictionaries_for_work_on_concrete_calculation
+import time
 
 coefficient_one = {"A240": 1.5, "A400": 2.5, "A500": 2.5}
 coefficient_two = {"ø <= 32": 1, "36 and 40": 0.9}
@@ -13,7 +14,7 @@ coefficient_two = {"ø <= 32": 1, "36 and 40": 0.9}
 # 0,9 - при диаметре арматуры 36 и 40 мм;
 
 # Rs: A240 = 210 МПа; A400: = 340 МПа; A500 = 435 МПа
-# В7,5 = 0,48; В10 = 0,56; В15 = 0,75; В20 = 0,9; В25 = 1,05; В30 = 1,15; В35 = 1,3; В40 = 1,4; В45 = 1,5; В50 = 1,6;
+# B3.5 = 0,26; B5 = 0,37; В7,5 = 0,48; В10 = 0,56; В15 = 0,75; В20 = 0,9; В25 = 1,05; В30 = 1,15; В35 = 1,3; В40 = 1,4; В45 = 1,5; В50 = 1,6;
 # В55 = 1,7; В60 = 1,8; В70	= 1,9; В80 = 2,1; В90 = 2,15; В100 = 2,2.
 
 # Функция concrete_class_check проверяет, что значение класса бетона введенное пользователем истино (существует ли в словаре) 
@@ -26,6 +27,7 @@ def concrete_class_check():
         if concrete_class_input in dictionaries_for_work_on_concrete_calculation.concrete_class_Rt:
             return dictionaries_for_work_on_concrete_calculation.concrete_class_Rt.get(concrete_class_input)
         else:
+            time.sleep(1)
             print('Неверно веден класс бетона, введите корректное значение')
             continue
 
@@ -39,7 +41,8 @@ def reinforcement_class_check():
         if reinforcement_class_input in dictionaries_for_work_on_concrete_calculation.reinforcement_class:
             return dictionaries_for_work_on_concrete_calculation.reinforcement_class.get(reinforcement_class_input), reinforcement_class_input
         else:
-            print('\nНеверно веден класс арматуры, введите корректное значение')
+            time.sleep(1)
+            print('Неверно веден класс арматуры, введите корректное значение')
             continue
 
 # Функция tension_anchorage_length делает расчет базовой длины анкеровки, небходимой для расчетной анкеровки, которая вложена в программу Anker.py 
@@ -49,17 +52,23 @@ def tension_anchorage_length():
     reinforcement, reinforcement_class_input = reinforcement_class_check()
     while True:
         rebar_diameter = input("\nВведите диаметр арматуры (до 40Ø)    ")
-        if rebar_diameter in str(list(range(1, 41))):
-            As = round(((math.pi * int(rebar_diameter) ** 2) / 4), 2)
-            μs = round((math.pi * int(rebar_diameter)), 2)
-            if int(rebar_diameter) <= 32:
-                Rbond = coefficient_one[reinforcement_class_input] * coefficient_two["ø <= 32"] * concrete
-                L0an = (reinforcement * As)/(Rbond * μs)
-                return L0an
-            elif 36 <= int(rebar_diameter) <= 40:
-                Rbond = coefficient_one[reinforcement_class_input] * coefficient_two["36 and 40"] * concrete
-                L0an = (reinforcement * As)/(Rbond * μs)
-                return L0an
+        try:
+            if rebar_diameter in str(list(range(1, 41))):
+                As = round(((math.pi * int(rebar_diameter) ** 2) / 4), 2)
+                μs = round((math.pi * int(rebar_diameter)), 2)
+                if int(rebar_diameter) <= 32:
+                    Rbond = coefficient_one[reinforcement_class_input] * coefficient_two["ø <= 32"] * concrete
+                    L0an = (reinforcement * As)/(Rbond * μs)
+                    return L0an
+                elif 36 <= int(rebar_diameter) <= 40:
+                    Rbond = coefficient_one[reinforcement_class_input] * coefficient_two["36 and 40"] * concrete
+                    L0an = (reinforcement * As)/(Rbond * μs)
+                    return L0an
+        except:
+            time.sleep(1)
+            print ('Неверно веден диаметр арматуры, введите корректное значение')
+            continue
         else:
-            print ('\nНеверно веден диаметр арматуры, введите корректное значение')
+            time.sleep(1)
+            print ('Неверно веден диаметр арматуры, введите корректное значение')
             continue
